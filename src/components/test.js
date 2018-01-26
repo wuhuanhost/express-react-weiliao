77814519293ebcf3d5a60cadc684822e62a4fefb
wuhuanhost <post.thinking@gmail.com>
2018-01-26 17:01:01 +0800

    update project......

--- src/components/test.js
+++ src/components/test.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import {observer} from 'mobx-react';
+import { observer } from 'mobx-react';
 
-@observer class MyComponent extends Component {
-    render() {
-        const {todos, user} = this.props;
-        return (<div>
-            {user.name}
-            <TodosView todos={todos} />
-        </div>)
-    }
+@observer
+class MyComponent extends Component {
+  render() {
+    return <div>123</div>;
+  }
 }
+
+
+
+export default MyComponent;
